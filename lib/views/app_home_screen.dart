12bc79f5f460e2405d25ef1975ca94624448dd38
Prathisthapandey12 <2022csb1105@iitrpr.ac.in>
2025-03-18 12:52:43 +0530

    updated home screen

--- lib/views/app_home_screen.dart
+++ lib/views/app_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:recipe_app/widgets/my_icon_button.dart';
+import 'package:recipe_app/widgets/banner.dart';
 class AppHomeScreen extends StatefulWidget{
   const AppHomeScreen({Key? key}) : super(key: key);
 
@@ -12,7 +13,49 @@ class _AppHomeScreenState extends State<AppHomeScreen>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
+      body : SafeArea(child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children : [
+              Padding(
+                padding : EdgeInsets.symmetric(horizontal: 20),
+                child : Column(
+                  children: [
+                    Row(children: [
+                      Text('What are you \n cooking today?', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold,height: 1,),),
+                      const Spacer(),
+                      MyIconButton(icon: Icons.notifications, pressed: (){},),
+
+                    ],),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 22),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: InputBorder.none,
+                          hintText : 'Search any recipe',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    BannerToExplore(),
+                ],)
+              ),
+              
+          ]
+        ),
+      ),
+      ),
     );
   }
 }
---
+++ lib/widgets/banner.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BannerToExplore extends StatelessWidget {
+  const BannerToExplore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: const Color.fromARGB(255, 12, 96, 15),
+      ),
+      child: Stack(
+        children : [
+          Positioned(
+            top : 30,
+            left : 15,
+            child : Column(children: [
+              Text('Explore new recipes', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white, height : 1.1),),
+              SizedBox(height: 10,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 33),
+                  backgroundColor: Colors.white,
+                  elevation: 0,
+                ),
+                onPressed: (){},
+                child: Text("Explore",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold, color : Colors.black,),)
+              ),
+          ],)
+          ),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            right: -30,
+            child: Image.asset('assets/images/cheff.png'),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
